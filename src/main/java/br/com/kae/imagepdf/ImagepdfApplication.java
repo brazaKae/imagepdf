a65f97ceb377b7f1046283f20af3903b36a2b99b
Kaê Renato Brazauskas <brazakae@gmail.com>
2025-03-30 23:12:24 -0300

    Initial commit

---
+++ src/main/java/br/com/kae/imagepdf/ImagepdfApplication.java
@@ -0,0 +1,13 @@
+package br.com.kae.imagepdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagepdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagepdfApplication.class, args);
+	}
+
+}
